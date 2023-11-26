b10565c5efc830e0892c60f8a00bcfd416bda042
pujan tamang <pujantamang92@gmail.com>
2023-11-26 19:13:28 +0000

    added explicit datatype with syntatic sugar expression

--- main.go
+++ main.go
@@ -7,12 +7,22 @@ func main() {
 		func main is the entrypoint for our package.
 	*/
 
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	// syntatic sugar with : create assigning variable easier
+	conferenceName := "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
 
+	fmt.Printf("conferenceName is of type %T, conferenceTickrts is of type %T and remainingTickets of type %T\n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets with %v still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// can be used later
+
+	userName = "Pujan"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
