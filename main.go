e54c19919e95b811d1ce980e8723f7528b3fe738
pujan tamang <pujantamang92@gmail.com>
2023-11-26 18:07:34 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		func main is the entrypoint for our package.
+	*/
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
+}
